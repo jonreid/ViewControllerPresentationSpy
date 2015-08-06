dfbfcd0f60fb92421c9bab3dda285296375696f8
Jon Reid <jon@qualitycoding.org>
2015-08-05 20:09:23 -0700

    Fix file comment header

--- TestSupport/QCOMockAlertController.h
+++ TestSupport/QCOMockAlertController.h
@@ -1,4 +1,4 @@
-//  MockAlerts by Jon Reid, http://qualitycoding.org/about/
+//  MockUIAlertController by Jon Reid, http://qualitycoding.org/about/
 //  Copyright 2015 Jonathan M. Reid. See LICENSE.txt
 
 #import <UIKit/UIKit.h>
