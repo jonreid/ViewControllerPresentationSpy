88e926eb7428be2426ffbe6874fbbb9bd64b0825
Tom Bates <tom.bates@sporthub.io>
2016-05-19 13:32:59 +0100

    Fixed swizzle always calling Notification

--- Sample/Tests/ViewControllerTests-PlainXCTest.m
+++ Sample/Tests/ViewControllerTests-PlainXCTest.m
@@ -256,4 +256,13 @@
     XCTAssertEqual(alertVerifier.popover.permittedArrowDirections, UIPopoverArrowDirectionAny);
 }
 
+- (void)testShowModalController_NotTracked
+{
+  QCOMockAlertVerifier *alertVerifier = [[QCOMockAlertVerifier alloc] init];
+  
+  [sut showModalController:nil];
+
+  XCTAssertNil(alertVerifier.message);
+}
+
 @end
