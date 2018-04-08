6faeec02ad62b1ce8a445f0bda6fc29e80c851c4
Jon Reid <jon@qualitycoding.org>
2018-04-07 18:48:11 -0700

    Add project to create module

---
+++ Source/MockUIAlertController/MockUIAlertController.h
@@ -0,0 +1,9 @@
+//  MockUIAlertController by Jon Reid, https://qualitycoding.org/
+//  Copyright 2018 Jonathan M. Reid. See LICENSE.txt
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double MockUIAlertControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char MockUIAlertControllerVersionString[];
+
+#import <MockUIAlertController/QCOMockAlertVerifier.h>
