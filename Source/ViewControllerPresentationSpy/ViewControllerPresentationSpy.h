44186e0ce2d75156779f9ee7f2309890082830d9
Jon Reid <jon@qualitycoding.org>
2022-01-15 12:00:39 -0800

    e Add NSObject extension to framework header

--- Source/ViewControllerPresentationSpy/ViewControllerPresentationSpy.h
+++ Source/ViewControllerPresentationSpy/ViewControllerPresentationSpy.h
@@ -6,6 +6,7 @@
 FOUNDATION_EXPORT double ViewControllerPresentationSpyVersionNumber;
 FOUNDATION_EXPORT const unsigned char ViewControllerPresentationSpyVersionString[];
 
+#import "NSObject+QCOMockAlerts.h"
 #import "QCOMockPopoverPresentationController.h"
 #import "UIAlertAction+QCOMock.h"
 #import "UIAlertController+QCOMock.h"
