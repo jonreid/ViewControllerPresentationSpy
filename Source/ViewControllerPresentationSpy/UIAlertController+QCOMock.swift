8f8bd1126c8ff1cb0f9eb705b48cd53d56ea6653
Jon Reid <jon@qualitycoding.org>
2022-01-15 11:49:41 -0800

    F qcoMock2_swizzle calls original

--- Source/ViewControllerPresentationSpy/UIAlertController+QCOMock.swift
+++ Source/ViewControllerPresentationSpy/UIAlertController+QCOMock.swift
@@ -2,6 +2,12 @@ import UIKit
 
 extension UIAlertController {
     
-@objc class func qcoMock2_swizzle() {}
+@objc class func qcoMock2_swizzle() {
+    UIAlertController.qcoMock_swizzle()
+
+//    NSObject.qcoMockAlerts_replaceClassMet
+//    [self qcoMockAlerts_replaceClassMethod:@selector(alertControllerWithTitle:message:preferredStyle:)
+//    withMethod:@selector(qcoMock_alertControllerWithTitle:message:preferredStyle:)];
+}
 
 }
