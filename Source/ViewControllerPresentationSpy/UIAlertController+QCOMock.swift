7dd35cf00aa69f7e01e75339e025bdf055a4b3ae
Jon Reid <jon@qualitycoding.org>
2022-01-22 11:24:13 -0800

    F!! UIAlertController swizzle2 now works!

--- Source/ViewControllerPresentationSpy/UIAlertController+QCOMock.swift
+++ Source/ViewControllerPresentationSpy/UIAlertController+QCOMock.swift
@@ -1,23 +1,18 @@
 import UIKit
 
 extension UIAlertController {
-    
-@objc class func qcoMock_swizzle2() {
-    NSObject.qcoMockAlerts_replaceClassMethod(
-            #selector(UIAlertController.init(title:message:preferredStyle:)),
-            withMethod: #selector(UIAlertController.qcoMock_alertController(withTitle:message:preferredStyle:))
-    )
-    
-    #if (os(iOS))
-        NSObject.qcoMockAlerts_replaceInstanceMethod(
-                #selector(getter: UIAlertController.popoverPresentationController),
-                withMethod: #selector(UIAlertController.qcoMock_popoverPresentationController)
+
+    @objc class func qcoMock_swizzle2() {
+        UIAlertController.qcoMockAlerts_replaceClassMethod(
+                #selector(UIAlertController.init(title:message:preferredStyle:)),
+                withMethod: #selector(UIAlertController.qcoMock_alertController(withTitle:message:preferredStyle:))
         )
-    #endif
 
-//    NSObject.qcoMockAlerts_replaceClassMet
-//    [self qcoMockAlerts_replaceClassMethod:@selector(alertControllerWithTitle:message:preferredStyle:)
-//    withMethod:@selector(qcoMock_alertControllerWithTitle:message:preferredStyle:)];
-}
-
+        #if (os(iOS))
+            UIAlertController.qcoMockAlerts_replaceInstanceMethod(
+                    #selector(getter: UIAlertController.popoverPresentationController),
+                    withMethod: #selector(UIAlertController.qcoMock_popoverPresentationController)
+            )
+        #endif
+    }
 }
