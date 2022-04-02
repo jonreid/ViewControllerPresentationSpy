4fb4e8fdd0b66014c68ffe89fe806371cb9b4ce3
Jon Reid <jon@qualitycoding.org>
2022-04-02 11:38:44 -0700

    R Delete UIAlertController+QCOMock.h

--- Source/ViewControllerPresentationSpy/UIAlertController+QCOMock.h
+++
@@ -1,9 +0,0 @@
-//  ViewControllerPresentationSpy by Jon Reid, https://qualitycoding.org
-//  Copyright 2022 Jonathan M. Reid. See LICENSE.txt
-
-#import <UIKit/UIKit.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-
-NS_ASSUME_NONNULL_END
