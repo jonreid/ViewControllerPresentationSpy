dbec18952d4d38cc65551b3aee4bd03233216ff5
Jon Reid <jon@qualitycoding.org>
2022-04-09 12:08:54 -0700

    R Swift swizzle now does the work (UIAlertAction)

--- Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.swift
+++ Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.swift
@@ -2,10 +2,9 @@ import UIKit
 
 extension UIAlertAction {
     @objc class func qcoMock_swizzle2() {
-        UIAlertAction.qcoMock_swizzle()
-//        UIAlertAction.qcoMockAlerts_replaceClassMethod(
-//                #selector(UIAlertAction.init(title:style:handler:)),
-//                withMethod: #selector(UIAlertAction.qcoMock_action(withTitle:style:handler:))
-//        )
+        UIAlertAction.qcoMockAlerts_replaceClassMethod(
+                #selector(UIAlertAction.init(title:style:handler:)),
+                withMethod: #selector(UIAlertAction.qcoMock_action(withTitle:style:handler:))
+        )
     }
 }
