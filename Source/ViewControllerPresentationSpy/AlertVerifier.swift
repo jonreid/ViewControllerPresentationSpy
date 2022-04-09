5b67f5fd4197ee707bfbb5e6d5f07c8ac8e426b8
Jon Reid <jon@qualitycoding.org>
2022-04-09 12:10:18 -0700

    R Rename swizzle2 to swizzle

--- Source/ViewControllerPresentationSpy/AlertVerifier.swift
+++ Source/ViewControllerPresentationSpy/AlertVerifier.swift
@@ -68,7 +68,7 @@ public class AlertVerifier: NSObject {
     }
 
     private static func swizzleMocks() {
-        UIAlertAction.qcoMock_swizzle2()
+        UIAlertAction.qcoMock_swizzle()
         UIAlertController.qcoMock_swizzle()
         UIViewController.qcoMock_swizzleCaptureAlert()
         AlertVerifier.isSwizzled.toggle()
--- Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.swift
+++ Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 extension UIAlertAction {
-    @objc class func qcoMock_swizzle2() {
+    @objc class func qcoMock_swizzle() {
         UIAlertAction.qcoMockAlerts_replaceClassMethod(
                 #selector(UIAlertAction.init(title:style:handler:)),
                 withMethod: #selector(UIAlertAction.qcoMock_action(withTitle:style:handler:))
