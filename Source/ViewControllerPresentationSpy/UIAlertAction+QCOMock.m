e02fd0e4ae6373b9e15d4157b9c6df04ac1ec388
Jon Reid <jon@qualitycoding.org>
2022-05-07 11:53:11 -0700

    R goodbye commented-out code

--- Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.m
+++ Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.m
@@ -17,14 +17,12 @@ void *const foo = @"foo";
 {
     UIAlertAction *action = [self qcoMock_actionWithTitle:title style:style handler:handler];
     UIAlertActionExtraProperties *extraProperties = [[UIAlertActionExtraProperties alloc] initWithHandler:handler];
-//    objc_setAssociatedObject(action, @selector(qcoMock_handler), handler, OBJC_ASSOCIATION_COPY_NONATOMIC);
     objc_setAssociatedObject(action, foo, extraProperties, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     return action;
 }
 
 - (void (^ __nullable)(UIAlertAction *action))qcoMock_handler
 {
-//    return objc_getAssociatedObject(self, @selector(qcoMock_handler));
     UIAlertActionExtraProperties *foobar = objc_getAssociatedObject(self, foo);
     return foobar.handler;
 }
