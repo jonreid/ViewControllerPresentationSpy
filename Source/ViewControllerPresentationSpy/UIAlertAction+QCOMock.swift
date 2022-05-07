894c8690dcf5936dddaffcd1f30610c69e93cc17
Jon Reid <jon@qualitycoding.org>
2022-05-07 11:59:01 -0700

    F Switch UIAlertAction to Swift swizzled init

--- Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.swift
+++ Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.swift
@@ -7,18 +7,9 @@ extension UIAlertAction {
     @objc class func qcoMock_swizzle() {
         UIAlertAction.qcoMockAlerts_replaceClassMethod(
             #selector(UIAlertAction.init(title:style:handler:)),
-            withMethod: #selector(UIAlertAction.qcoMock_action(withTitle:style:handler:))
+            withMethod: #selector(UIAlertAction.qcoMock_action2(withTitle:style:handler:))
         )
     }
-
-    // + (instancetype)qcoMock_actionWithTitle:(NSString *)title
-    //        style:(UIAlertActionStyle)style
-    //        handler:(void (^ __nullable)(UIAlertAction *action))handler
-    // {
-    //        UIAlertAction *action = [self qcoMock_actionWithTitle:title style:style handler:handler];
-    //        objc_setAssociatedObject(action, @selector(qcoMock_handler), handler, OBJC_ASSOCIATION_COPY_NONATOMIC);
-    // return action;
-    // }
 
     @objc class func qcoMock_action2(withTitle title: String, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)?) -> UIAlertAction {
         let action = UIAlertAction.qcoMock_action2(withTitle: title, style: style, handler: handler)
