6e65d76a5d36ca7892d8937b5c3fe17cc2bea06b
Jon Reid <jon@qualitycoding.org>
2022-05-14 12:02:28 -0700

    R Remove @objc from UIAlertAction

--- Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.swift
+++ Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.swift
@@ -4,7 +4,7 @@
 import UIKit
 
 extension UIAlertAction {
-    @objc class func qcoMock_swizzle() {
+    class func qcoMock_swizzle() {
         UIAlertAction.qcoMockAlerts_replaceClassMethod(
             #selector(UIAlertAction.init(title:style:handler:)),
             withMethod: #selector(UIAlertAction.qcoMock_action(withTitle:style:handler:))
