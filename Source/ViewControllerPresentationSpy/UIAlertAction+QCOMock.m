c4b2c42c12b39b3aa00e3a4b6a000ac761b7d27b
Jon Reid <jon@qualitycoding.org>
2022-05-14 11:53:21 -0700

    R Delete UIAlertAction Obj-C stuff!

--- Source/ViewControllerPresentationSpy/UIAlertAction+QCOMock.m
+++
@@ -1,8 +0,0 @@
-//  ViewControllerPresentationSpy by Jon Reid, https://qualitycoding.org
-//  Copyright 2022 Jonathan M. Reid. See LICENSE.txt
-
-#import "UIAlertAction+QCOMock.h"
-
-#import "ViewControllerPresentationSpy/ViewControllerPresentationSpy-Swift.h"
-#import <objc/runtime.h>
-
