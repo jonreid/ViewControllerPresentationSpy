2e250e32449f51b0f16a054a5e5363632649cc1c
Jon Reid <jon@qualitycoding.org>
2022-05-21 11:39:33 -0700

    R Create empty extension for UIViewController

---
+++ Source/ViewControllerPresentationSpy/UIViewController+QCOMock.swift
@@ -0,0 +1,13 @@
+//  ViewControllerPresentationSpy by Jon Reid, https://qualitycoding.org
+//  Copyright 2022 Jonathan M. Reid. See LICENSE.txt
+
+import UIKit
+
+extension UIViewController {
+//    class func qcoMock_swizzle() {
+//        UIAlertAction.qcoMockAlerts_replaceClassMethod(
+//                #selector(UIAlertAction.init(title:style:handler:)),
+//                withMethod: #selector(UIAlertAction.qcoMock_action(withTitle:style:handler:))
+//        )
+//    }
+}
