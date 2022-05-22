933a1afe5f2979a24ba9f9994fcdebcac9139554
Jon Reid <jon@qualitycoding.org>
2022-05-22 16:27:10 -0700

    a Auto-format merge_dependabot.sh

--- merge_dependabot.sh
+++ merge_dependabot.sh
@@ -2,8 +2,7 @@
 
 gh pr merge --auto --rebase "$PR_URL"
 
-if [ $? -ne 0 ]
-then
-    echo 'If you want GitHub to automatically merge pull requests from Dependabot, you need to create a'
-    echo 'personal access token (public_repo) and assign it to Settings -> Secrets -> Dependabot -> GH_ACTION_TOKEN'
+if [ $? -ne 0 ]; then
+  echo 'If you want GitHub to automatically merge pull requests from Dependabot, you need to create a'
+  echo 'personal access token (public_repo) and assign it to Settings -> Secrets -> Dependabot -> GH_ACTION_TOKEN'
 fi
