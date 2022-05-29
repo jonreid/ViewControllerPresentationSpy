98a9972b163473d1bb3442b47e97be5a1faabe3a
Jon Reid <jon@qualitycoding.org>
2022-05-29 16:22:45 -0700

    d - Update file comment header with license link and SPDX-License-Identifier

--- Source/ViewControllerPresentationSpy/NSObject+QCOMockAlerts.h
+++ Source/ViewControllerPresentationSpy/NSObject+QCOMockAlerts.h
@@ -1,5 +1,6 @@
-//  ViewControllerPresentationSpy by Jon Reid, https://qualitycoding.org
-//  Copyright 2022 Jonathan M. Reid. See LICENSE.txt
+// ViewControllerPresentationSpy by Jon Reid, https://qualitycoding.org
+// Copyright 2022 Jonathan M. Reid. https://github.com/jonreid/ViewControllerPresentationSpy/blob/main/LICENSE.txt
+// SPDX-License-Identifier: MIT
 
 #import <Foundation/Foundation.h>
 
