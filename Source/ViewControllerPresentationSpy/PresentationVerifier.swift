4a50dfea3fc962cc0ec57d982ecd13d7b44dcfcc
Jon Reid <jon@qualitycoding.org>
2022-06-11 10:51:27 -0700

    a - SwiftFormat on PresentationVerifier.swift

--- Source/ViewControllerPresentationSpy/PresentationVerifier.swift
+++ Source/ViewControllerPresentationSpy/PresentationVerifier.swift
@@ -2,12 +2,12 @@
 // Copyright 2022 Jonathan M. Reid. https://github.com/jonreid/ViewControllerPresentationSpy/blob/main/LICENSE.txt
 // SPDX-License-Identifier: MIT
 
-import XCTest
 import UIKit
+import XCTest
 
 /**
     Captures presented view controllers.
- 
+
     Instantiate a PresentationVerifier before the execution phase of the test. Then invoke the code
     to create and present your view controller. Information about the presentation is then available
     through the PresentationVerifier.
@@ -16,49 +16,49 @@ import UIKit
 public class PresentationVerifier: NSObject {
     /// Number of times present(_:animated:completion:) was called.
     @objc public var presentedCount = 0
-    
+
     @objc public var presentedViewController: UIViewController?
     @objc public var presentingViewController: UIViewController?
     @objc public var animated: Bool = false
-    
+
     /// Production code completion handler passed to present(_:animated:completion:).
     @objc public var capturedCompletion: (() -> Void)?
-    
+
     /// Test code can provide its own completion handler to fulfill XCTestExpectations.
     @objc public var testCompletion: (() -> Void)?
 
-    static private(set) var isSwizzled = false
+    private(set) static var isSwizzled = false
 
     /**
         Initializes a newly allocated verifier.
-     
+
         Instantiating a PresentationVerifier swizzles UIViewController. It remains swizzled until
         the PresentationVerifier is deallocated. Only one PresentationVerifier may exist at a time,
         and none may be created while an AlertVerifier exists. (This is because they both swizzle
         UIViewController.)
      */
-    @objc public override init() {
+    @objc override public init() {
         super.init()
         guard !PresentationVerifier.isSwizzled else {
             XCTFail("""
-                    More than one instance of PresentationVerifier exists. This may be caused by \
-                    creating one setUp() but failing to set the property to nil in tearDown().
-                    """)
+            More than one instance of PresentationVerifier exists. This may be caused by \
+            creating one setUp() but failing to set the property to nil in tearDown().
+            """)
             return
         }
         guard !AlertVerifier.isSwizzled else {
             XCTFail("""
-                    A PresentationVerifier may not be created while an AlertVerifier exists. Try \
-                    making the AlertVerifier optional, and setting it to nil before creating the \
-                    PresentationVerifier.
-                    """)
+            A PresentationVerifier may not be created while an AlertVerifier exists. Try \
+            making the AlertVerifier optional, and setting it to nil before creating the \
+            PresentationVerifier.
+            """)
             return
         }
         NotificationCenter.default.addObserver(
-                self,
-                selector: #selector(viewControllerWasPresented(_:)),
-                name: NSNotification.Name.QCOMockViewControllerPresented,
-                object: nil
+            self,
+            selector: #selector(viewControllerWasPresented(_:)),
+            name: NSNotification.Name.QCOMockViewControllerPresented,
+            object: nil
         )
         PresentationVerifier.swizzleMocks()
     }
@@ -86,25 +86,25 @@ public class PresentationVerifier: NSObject {
     }
 }
 
-extension PresentationVerifier {
+public extension PresentationVerifier {
     /**
-        Verifies presentation of one view controller.
-    */
-    @discardableResult public func verify<VC: UIViewController>(
-            animated: Bool,
-            presentingViewController: UIViewController? = nil,
-            file: StaticString = #file,
-            line: UInt = #line
+         Verifies presentation of one view controller.
+     */
+    @discardableResult func verify<VC: UIViewController>(
+        animated: Bool,
+        presentingViewController: UIViewController? = nil,
+        file: StaticString = #file,
+        line: UInt = #line
     ) -> VC? {
-        let abort = verifyCallCount(actual: self.presentedCount, action: "present", file: file, line: line)
+        let abort = verifyCallCount(actual: presentedCount, action: "present", file: file, line: line)
         if abort { return nil }
         verifyAnimated(actual: self.animated, expected: animated, action: "present", file: file, line: line)
         verifyViewController(actual: self.presentingViewController, expected: presentingViewController,
-                adjective: "presenting", file: file, line: line)
+                             adjective: "presenting", file: file, line: line)
         let nextVC = presentedViewController as? VC
         if nextVC == nil {
             XCTFail("Expected presented view controller to be \(VC.self)), " +
-                    "but was \(String(describing: presentedViewController))", file: file, line: line)
+                "but was \(String(describing: presentedViewController))", file: file, line: line)
         }
         return nextVC
     }
