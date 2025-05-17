9c57400e3136f50a1cc9f15d0c834e4203265028
Jon Reid <jon@qualitycoding.org>
2025-05-16 21:50:05 -0700

    . d Change copyright notice to original year

--- Source/ViewControllerPresentationSpy/NSObject+Swizzling.swift
+++ Source/ViewControllerPresentationSpy/NSObject+Swizzling.swift
@@ -1,3 +1,7 @@
+// ViewControllerPresentationSpy by Jon Reid, https://qualitycoding.org
+// Copyright 2015 Jonathan M. Reid. https://github.com/jonreid/ViewControllerPresentationSpy/blob/main/LICENSE.txt
+// SPDX-License-Identifier: MIT
+
 import Foundation
 
 extension NSObject {
