179f38111d6d2e2f2bd543388eb8188c49687bfb
Jon Reid <jon@qualitycoding.org>
2025-11-21 11:01:17 -0800

    . e Update action to merge dependabot PRs

--- merge_dependabot.sh
+++ merge_dependabot.sh
@@ -1,8 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
 
-gh pr merge --auto --rebase "$PR_URL"
-
-if [ $? -ne 0 ]; then
-  echo 'If you want GitHub to automatically merge pull requests from Dependabot, you need to create a'
-  echo 'personal access token (public_repo) and assign it to Settings -> Secrets -> Dependabot -> GH_ACTION_TOKEN'
-fi
+GH_TOKEN="$GITHUB_TOKEN" gh pr merge --auto --rebase "$PR_URL"
