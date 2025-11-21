8268b7ff1b3838e35a7f2c0e6fbca0d0a4d05e56
Jon Reid <jon@qualitycoding.org>
2025-11-21 12:17:15 -0800

    . e Update runtests script

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-set -e # exit when any command fails
+echo "Source"
+xcodebuild build -project 'Source/ViewControllerPresentationSpy.xcodeproj' -scheme 'ViewControllerPresentationSpy-tvOS' -sdk 'appletvsimulator' -destination 'platform=tvOS Simulator,OS=18.5,name=Apple TV' | xcbeautify
 
-set -o pipefail && xcodebuild build -project 'Source/ViewControllerPresentationSpy.xcodeproj' -scheme 'ViewControllerPresentationSpy-tvOS' -sdk 'appletvsimulator' -destination 'platform=tvOS Simulator,OS=18.5,name=Apple TV' | xcbeautify
-set -o pipefail && xcodebuild test -project 'ObjCSample/ObjCSampleViewControllerPresentationSpy.xcodeproj' -scheme 'ObjCSampleViewControllerPresentationSpy' -sdk 'iphonesimulator' -destination 'platform=iOS Simulator,OS=18.5,name=iPhone 16' | xcbeautify
-set -o pipefail && xcodebuild test -project 'SwiftSample/SwiftSampleViewControllerPresentationSpy.xcodeproj' -scheme 'SwiftSampleViewControllerPresentationSpy' -sdk 'iphonesimulator' -destination 'platform=iOS Simulator,OS=18.5,name=iPad Air (4th generation)' | xcbeautify
-set -o pipefail && xcodebuild test -project 'SwiftSamplePackage/SwiftSampleViewControllerPresentationSpy.xcodeproj' -scheme 'SwiftSampleViewControllerPresentationSpy' -sdk 'iphonesimulator' -destination 'platform=iOS Simulator,OS=18.5,name=iPad Air (4th generation)' | xcbeautify
+echo "========"
+echo "ObjCSample"
+xcodebuild test -project 'ObjCSample/ObjCSampleViewControllerPresentationSpy.xcodeproj' -scheme 'ObjCSampleViewControllerPresentationSpy' -sdk 'iphonesimulator' -destination 'platform=iOS Simulator,OS=18.5,name=iPhone 16' | xcbeautify
+
+echo "========"
+echo "SwiftSample"
+xcodebuild test -project 'SwiftSample/SwiftSampleViewControllerPresentationSpy.xcodeproj' -scheme 'SwiftSampleViewControllerPresentationSpy' -sdk 'iphonesimulator' -destination 'platform=iOS Simulator,OS=18.5,name=iPad Air (4th generation)' | xcbeautify
+
+echo "========"
+echo "SwiftSamplePackage"
+xcodebuild test -project 'SwiftSamplePackage/SwiftSampleViewControllerPresentationSpy.xcodeproj' -scheme 'SwiftSampleViewControllerPresentationSpy' -sdk 'iphonesimulator' -destination 'platform=iOS Simulator,OS=18.5,name=iPad Air (4th generation)' | xcbeautify
